a9bd94ad12ac38a85c9ac7899b446e2ce2ec0c2c
roshan <roshangupta.7997@gmail.com>
2025-09-04 09:00:12 +0530

    add excel driven testing

--- src/MapAPI_POJO_Serialization.java
+++ src/MapAPI_POJO_Serialization.java
@@ -9,10 +9,13 @@ import mapPojo.Location;
 import static io.restassured.RestAssured.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MapAPI_POJO_Serialization {
-	AddPlace p = new AddPlace();
+	AddPlace pojoMap = new AddPlace();
+	HashMap<String, Object> jsonMap = new HashMap<>();
 	@BeforeClass
 	public void setup() {
 		RestAssured.baseURI = "https://rahulshettyacademy.com";
@@ -28,20 +31,48 @@ public class MapAPI_POJO_Serialization {
 		l.setLat(-38.383494);
 		l.setLng(33.427362);
 		
-		p.setAccuracy(50);
-		p.setAddress("29, side layout, cohen 09");
-		p.setLanguage("French-IN");
-		p.setName("Frontline house");
-		p.setPhone_number("(+91) 983 893 3937");
-		p.setWebsite("http://google.com");
-		p.setTypes(myList);
-		p.setLocation(l);
+		pojoMap.setAccuracy(50);
+		pojoMap.setAddress("29, side layout, cohen 09");
+		pojoMap.setLanguage("French-IN");
+		pojoMap.setName("Frontline house");
+		pojoMap.setPhone_number("(+91) 983 893 3937");
+		pojoMap.setWebsite("http://google.com");
+		pojoMap.setTypes(myList);
+		pojoMap.setLocation(l);
+		
+//		BY 	USING HASH MAP
+		// Outer map
+//        HashMap<String, Object> jsonMap = new HashMap<>();
+
+        // Nested "location" map
+        HashMap<String, Object> locationMap = new HashMap<>();
+        locationMap.put("lat", -38.383494);
+        locationMap.put("lng", 33.427362);
+
+        // Add location to outer map
+        jsonMap.put("location", locationMap);
+
+        // Add simple key-value pairs
+        jsonMap.put("accuracy", 50);
+        jsonMap.put("name", "Frontline house");
+        jsonMap.put("phone_number", "(+91) 983 893 3937");
+        jsonMap.put("address", "29, side layout, cohen 09");
+
+        // Add array (list in Java)
+        List<String> typesList = new ArrayList<>();
+        typesList.add("Bottle park");
+        typesList.add("shop");
+        jsonMap.put("types", typesList);
+
+        jsonMap.put("website", "http://google.com");
+        jsonMap.put("language", "French-IN");
+
 	}
 	
 	@Test
 	public void addPlace() {
 		Response res = given().queryParam("key", "qaclick123")
-		.body(p)
+		.body(jsonMap).log().body()								//to uese pojo technique , give pojoMap variable in body
 		.when().post("/maps/api/place/add/json")
 		.then().assertThat().statusCode(200).extract().response();
 		
